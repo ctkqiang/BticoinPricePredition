9bcf38112bacf6d08ef94b9de29185eb827b506f
John Melody Me and Tan Sin Dee <johnmelodyme@icloud.com>
2020-04-03 00:24:07 +0800

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -5,13 +5,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import datetime
 import numpy as np
+from pandas.plotting import register_matplotlib_converters
 
+register_matplotlib_converters()
 bitcoin_market_info = pd.read_csv("data/bitcoinPriceData.csv")
 bitcoin_market_info["Timestamp"] = pd.to_datetime(bitcoin_market_info.Timestamp)
 bitcoin_market_info.set_index("Timestamp", inplace=True)
 date = bitcoin_market_info.resample("D").mean()
 print(bitcoin_market_info.head())
-plt.style.use("seaborn-poster")
+# plt.style.use("seaborn-poster")
 fig = plt.figure(figsize=[15, 7])
 plt.suptitle("Bitcoin Exchange mean $USD", fontsize=12)
 # plt.subplot(221)
